7da0af1c25d5a0818c97494a0d3fa2f7f33c2d5d
tuanemtramtinh <anh.nguyen2252038@hcmut.edu.vn>
2025-11-15 23:41:13 +0700

    update image limit size

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -11,7 +11,7 @@ const uploadMany = multer({
   storage: multer.memoryStorage(),
   limits: {
     files: 2,
-    fileSize: 3 * 1024 * 1024,
+    fileSize: 5 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
     const allowed = ["image/jpeg", "image/png", "image/webp"];
